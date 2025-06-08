83f6767386a5d6f301fce1ee3ed596e2c617f08d
Орел Михаил <77534887+m0khm@users.noreply.github.com>
2025-06-08 23:43:23 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include "film.h"
+#include "interface.h"
+#include <locale.h>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "Russian");
 
+    FilmDatabase database;
+
+    // грузим существующий файл (если есть)
+    if (!database.loadFromFile("films.dat")) {
+        cout << "Файл данных не найден. Будет создана новая база.\n";
+    }
+
+    UserInterface ui(database);
+    ui.run();            // вся работа пользователя
+
+   
+    if (!database.saveToFile("films.dat")) {
+        cerr << "Не удалось сохранить базу данных в файл films.dat\n";
+    }
+   
+
+    return 0;
+}
